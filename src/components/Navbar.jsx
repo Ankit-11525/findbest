d88c14a047dd78a4e0e9f31669d3f600d03d6e0e
Vineet-0 <vineet351016@gmail.com>
2023-07-05 14:38:25 +0530

    Navbar Home Category CSS --> Tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,11 +3,11 @@ import React from "react";
 const navbar = () => {
   return (
     <div>
-      <div className="flex h-14 items-center flex-row justify-between px-2 bg-slate-800">
-        <h2 className="font-bold text-white text-xl">#FindBest</h2>
+      <div className="flex h-16 items-center flex-row justify-between px-32 bg-sky-900">
+        <h2 className="font-bold text-white text-2xl">#FindBest</h2>
         <button>
           <a href="https://github.com" target="blank">
-            <img className="h-8" src="github-logo.png" alt="GitHub Logo" />
+            <img className="h-10" src="github-logo.png" alt="GitHub Logo" />
           </a>
         </button>
       </div>
